5461164cb6151ec324d7a69ac36ce6837ded0af9
Nicklas Rudolfsson <66663564+nirucon@users.noreply.github.com>
2024-08-06 15:04:49 +0200

    Update pipevoid-installer.sh

--- pipevoid-installer.sh
+++ pipevoid-installer.sh
@@ -1,28 +1,59 @@
 #!/bin/bash
 
-# pipevoid - pipewire installer for void linux by Nicklas Rudolfsson
+# pipevoid - PipeWire install script for Void Linux by Nicklas Rudolfsson
 
-# Update system and install necessary packages
+# Function to prompt the user for reboot
+prompt_reboot() {
+    read -r -p "It's recommended to reboot your system, do you want to do it now? Y/n: " response
+    case "$response" in
+        [Yy]* | "" )
+            echo "Rebooting system..."
+            sudo reboot
+            ;;
+        [Nn]* )
+            echo "Please remember to reboot your system later to apply changes."
+            ;;
+        * )
+            echo "Invalid input. Please enter Y or n."
+            prompt_reboot
+            ;;
+    esac
+}
+
+# Check if figlet is installed
+if command -v figlet &> /dev/null; then
+    figlet pipevoid
+else
+    echo "---------------------------"
+    echo "       pipevoid            "
+    echo "---------------------------"
+fi
+
+echo "Updating system and installing necessary packages..."
 sudo xbps-install -S dumb_runtime_dir pipewire wireplumber pavucontrol qpwgraph
 
-# Create directories and create symbolic links for PipeWire and WirePlumber configuration
+echo "Creating directories and symbolic links for PipeWire and WirePlumber configuration..."
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
 
-# Create symbolic links for autostart
+echo "Creating symbolic links for autostarting WirePlumber and PipeWire..."
 sudo ln -s /usr/share/applications/wireplumber.desktop /etc/xdg/autostart/
 sudo ln -s /usr/share/applications/pipewire.desktop /etc/xdg/autostart/
 
-# Backup existing .xinitrc if it exists
+echo "Checking for existing .xinitrc file and making a backup if it exists..."
 if [ -f ~/.xinitrc ]; then
     cp ~/.xinitrc ~/.xinitrc.bak
+    echo "Backup of existing .xinitrc created as .xinitrc.bak."
 fi
 
-# Insert PipeWire start commands into .xinitrc
+echo "Inserting PipeWire start commands into .xinitrc..."
 grep -qxF '# PipeWire start' ~/.xinitrc || echo -e "\n# PipeWire start\npipewire &\npipewire-pulse &\nwireplumber &" >> ~/.xinitrc
 
-# Insert dbus launch command at the end of .xinitrc
+echo "Appending dbus launch command at the end of .xinitrc..."
 grep -qxF '# Void dbus' ~/.xinitrc || echo -e "\n# Void dbus\nexec dbus-launch --sh-syntax --exit-with-session dwm" >> ~/.xinitrc
 
-echo "Installation and configuration completed. Please restart your session."
+echo "Installation and configuration completed."
+
+# Prompt the user to reboot
+prompt_reboot
