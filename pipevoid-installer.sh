14d8a19eb87595b5a1950245780a4e27d9328165
Nicklas Rudolfsson <66663564+nirucon@users.noreply.github.com>
2024-08-08 17:11:32 +0200

    Update pipevoid-installer.sh

--- pipevoid-installer.sh
+++ pipevoid-installer.sh
@@ -9,17 +9,18 @@ print_message() {
 
 # Update the system and install necessary packages
 print_message "Updating the system and installing necessary packages"
-xbps-install -Syu
-xbps-install -Sy pipewire alsa-utils alsa-plugins alsa-firmware pipewire-alsa pipewire-pulse wireplumber
+sudo xbps-install -Syu
+sudo xbps-install -Sy xorg-minimal xinit suckless-tools pipewire alsa-utils alsa-plugins alsa-firmware pipewire-pulse wireplumber pavucontrol volumeicon
 
 # Enable pipewire services
 print_message "Enabling Pipewire and WirePlumber services"
-ln -s /etc/sv/pipewire /var/service/
-ln -s /etc/sv/wireplumber /var/service/
+sudo ln -s /etc/sv/pipewire /var/service/
+sudo ln -s /etc/sv/wireplumber /var/service/
 
 # Configure ALSA to use Pipewire
 print_message "Configuring ALSA to use Pipewire"
-cat <<EOF > /etc/alsa/conf.d/99-pipewire-default.conf
+sudo mkdir -p /etc/alsa/conf.d
+sudo bash -c 'cat <<EOF > /etc/alsa/conf.d/99-pipewire-default.conf
 pcm.!default {
     type plug
     slave.pcm {
@@ -30,41 +31,41 @@ pcm.!default {
 ctl.!default {
     type pipewire
 }
-EOF
+EOF'
 
 # Reload ALSA
 print_message "Reloading ALSA"
-alsa force-reload
+sudo alsa force-reload
 
 # Add instructions for starting services and enabling audio on login
 print_message "Adding instructions for starting services on login"
-cat <<EOF >> /etc/profile
+sudo bash -c 'cat <<EOF >> /etc/profile
 # Start pipewire and wireplumber
 if [ -z "\$DISPLAY" ] && [ \$(tty) = /dev/tty1 ]; then
     exec startx
 fi
-EOF
+EOF'
 
 # Set up runit services for Pipewire
 print_message "Setting up runit services for Pipewire"
-mkdir -p /etc/sv/pipewire
-cat <<EOF > /etc/sv/pipewire/run
+sudo mkdir -p /etc/sv/pipewire
+sudo bash -c 'cat <<EOF > /etc/sv/pipewire/run
 #!/bin/sh
 exec pipewire
-EOF
-chmod +x /etc/sv/pipewire/run
+EOF'
+sudo chmod +x /etc/sv/pipewire/run
 
-mkdir -p /etc/sv/wireplumber
-cat <<EOF > /etc/sv/wireplumber/run
+sudo mkdir -p /etc/sv/wireplumber
+sudo bash -c 'cat <<EOF > /etc/sv/wireplumber/run
 #!/bin/sh
 exec wireplumber
-EOF
-chmod +x /etc/sv/wireplumber/run
+EOF'
+sudo chmod +x /etc/sv/wireplumber/run
 
 # Enable services
 print_message "Enabling Pipewire and Wireplumber services"
-ln -s /etc/sv/pipewire /var/service/pipewire
-ln -s /etc/sv/wireplumber /var/service/wireplumber
+sudo ln -s /etc/sv/pipewire /var/service/pipewire
+sudo ln -s /etc/sv/wireplumber /var/service/wireplumber
 
 # Notify user of completion
 print_message "Installation and configuration complete. Reboot your system."
